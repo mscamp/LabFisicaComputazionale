10775cfdf918597cabb7f4a5d62b7db7dbab4618
mscamp <m.scampini@proton.me>
2025-01-19 21:53:53 +0100

    Fixed typo

--- Monte Carlo/esercizio9/es9.h
+++ Monte Carlo/esercizio9/es9.h
@@ -4,6 +4,6 @@
 
 double rand_range(double a, double b, bool estremi);
 std::vector<double> rand_TCL(double a, double b, int M, int N, bool estremi);
-void scrivi_su_file(std::string file_name, std::vector<double> vec);
+void scrivi_su_file(std::string file_name, std::vector<double> v);
 double momento_k_campione(std::vector<double> sample, int k);
 std::pair<double, double> momenti_gaussiana(int N);
