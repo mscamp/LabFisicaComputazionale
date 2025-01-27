d978f49a121c7d39088a50eeab8e1f3cfa830648
mscamp <m.scampini@proton.me>
2025-01-27 22:37:49 +0100

    Esercizio 16c

---
+++ Equazioni differenziali/esercizio16/es16_c/functions.cc
@@ -0,0 +1,27 @@
+// Librerie
+#include <vector>
+#include <math.h>
+
+// Definizione delle componenti di f
+double f_0(double x, std::vector<double> y)
+{
+    return y.at(1);
+}
+
+double f_1(double x, std::vector<double> y)
+{
+    // Parametri
+    double alpha_par = 0.5;
+    double beta_par = 0.7;
+    double gamma_par = 0.03;
+
+    return (-1.0) * gamma_par * y.at(1) + (beta_par * cos(x) - alpha_par) * sin(y.at(0));
+}
+
+std::vector<double> f(double x, std::vector<double> y)
+{
+    // Componenti di f
+    std::vector<double> componenti = {f_0(x, y), f_1(x, y)};
+
+    return componenti;
+}
